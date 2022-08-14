795f2069570b905c5e6c3202763a775faea6efa6
annivadas <anni.vadas@gmail.com>
2022-08-14 13:34:35 +0200

    nem működik a teszt

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const handleClick = () => {
+  let elements = document.querySelectorAll('.button');
+  for (let i = 0; i < elements.length; i += 1) {
+    elements[i].addEventListener('click', () => console.log(elements[i].innerHTML)
+    )
+  }
+}
+handleClick()
+
